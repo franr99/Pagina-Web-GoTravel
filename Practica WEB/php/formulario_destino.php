3f7948985350f69b81db0d9eb70e6d93022a1160
franr99 <frosas@alu.ucam.edu>
2020-05-01 19:55:42 +0200

    mierda php

---
+++ Practica WEB/php/formulario_destino.php
@@ -0,0 +1,16 @@
+<?php
+$id = $_GET['idDestino'];
+$origen = $_GET['origen'];
+$f_ini = $_GET['fecha_ini'];
+$f_fin = $_GET['fecha_fin'];
+$compania = $_GET['compania'];
+
+
+
+echo "el id es: " . $id . " ";
+echo "el origen es: " . $origen . " ";
+echo "fecha ini es: " . $f_ini . " ";
+echo "fecha fin es: " . $f_fin . " ";
+echo "compania: " . $compania . " ";
+?>
+
