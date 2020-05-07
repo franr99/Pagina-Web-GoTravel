0201bd4c61339a1cb91cd78078667cc5d3486fc5
JJbestOf99 <56269172+JJbestOf99@users.noreply.github.com>
2020-05-07 12:13:53 +0200

    tour

--- Practica WEB/tours.php
+++ Practica WEB/tours.php
@@ -77,6 +77,8 @@
                         <li><a href="https://www.civitatis.com/es/bruselas/excursion-brujas"><img src="img/brujas-excur.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/paris/paseo-barco-sena"><img src="img/paris-excur.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/nueva-york/excursion-washington"><img src="img/washington-excur.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/madrid/excursion-toledo-segovia"><img src="img/fachada-catedral-segovia.jpg"></a></li>
+                        <li><a href="https://www.civitatis.com/es/amsterdam/excursion-zaanse-schans-volendam-marken"><img src="img/paises-bajos-volendam"></a></li>
                         
                     </ul>
 
@@ -95,6 +97,8 @@
                         <li><a href="https://www.civitatis.com/es/roma/tour-coliseo-arena-gladiadores"><img src="img/roma-cult.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/el-cairo/piramides-giza-menfis-saqqara"><img src="img/cairo-cult.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/paris/entrada-sainte-chapelle"><img src="img/sainte-chapelle-cult.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/granada/visita-guiada-alhambra/"><img src="img/alhambra.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/bali/tour-cultural-bali-kintamani"><img src="img/danza-barong.webp"></a></li>
                         
                     </ul>
 
@@ -113,6 +117,8 @@
                         <li><a href="https://www.civitatis.com/es/stavanger/senderismo-preikestolen-amanecer"><img src="img/amanecer-fiordo-aven.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/lucerna/excursion-monte-rigi"><img src="img/monte-rigi-aven.webp"></a></li>
                         <li><a href="https://www.civitatis.com/es/baqueira/paseo-raquetas-nieve-baqueira-beret"><img src="img/raquetas-nieve-aven.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/san-gil/rafting-rio-fonce"><img src="img/rio-fonce.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/tenerife/tour-kayak-punta-teno"><img src="img/kayak.webp"></a></li>
                         
                     </ul>
 
@@ -131,6 +137,8 @@
                         <li><a href="https://www.londres.es/museo-britanico"><img src="img/arte_londres.jpg"></a></li>
                         <li><a href="https://www.museodelprado.es/"><img src="img/arte_madrid.jpg"></a></li>
                         <li><a href="https://www.paris.es/museo-louvre"><img src="img/arte_paris.jpg"></a></li>
+                        <li><a href="https://www.civitatis.com/es/la-habana/clase-pintura-galeria-arte"><img src="img/cuadro-jose-marti.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/florencia/visita-guiada-florencia-uffizi-academia"><img src="img/nacimiento-venus.webp"></a></li>
                         
                     </ul>
 
@@ -149,6 +157,8 @@
                         <li><a href="https://www.elreyleon.es/"><img src="img/entretenimiento_madrid.jpg"></a></li>
                         <li><a href="https://es.uefa.com/uefaeuro-2020/"><img src="img/entretenimiento_euro.jpg"></a></li>
                         <li><a href="https://www.dakar.com/es"><img src="img/entretenimiento_paris.jpg"></a></li>
+                        <li><a href="https://www.civitatis.com/es/los-angeles/entrada-universal-studios-hollywood"><img src="img/universal-studios.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/san-petersburgo/entradas-ballet-ruso"><img src="img/ballet-cisnes.webp"></a></li>
                         
                     </ul>
 
@@ -167,6 +177,8 @@
                         <li><a href="https://www.culinaryspain.es/rutas-gastronomicas/rutas-gastronomicas-espana/barcelona-5-dias/"><img src="img/gastronomia_bar.jpg"></a></li>
                         <li><a href="https://www.culinaryspain.es/rutas-gastronomicas/rutas-gastronomicas-espana/madrid-gourmet-4-dias/"><img src="img/gastronomia_madrid.jpg"></a></li>
                         <li><a href="https://www.disfrutaberlin.com/actividades/tour-gastronomico-berlin"><img src="img/gastronomia_berlin.jpg"></a></li>
+                        <li><a href="https://www.civitatis.com/es/nueva-york/excursion-washington"><img src="img/washington-excur.webp"></a></li>
+                        <li><a href="https://www.civitatis.com/es/nueva-york/excursion-washington"><img src="img/washington-excur.webp"></a></li>
                         
                     </ul>
 
